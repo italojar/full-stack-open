6503b517b45870745397943c9b6c320b7afd6760
italojar <jaramilloarmijositalo@gmail.com>
2022-02-08 22:37:41 +0100

    Fixed Content component

--- part1/courseinfo/src/Content.js
+++ part1/courseinfo/src/Content.js
@@ -2,10 +2,11 @@
 const Part = ({part, exercise})  => <p>{part} {exercise} </p>
 
 const Content = (props) => {
+    console.log(props)
     return (
         <div>
           <Part part={props.part1} exercise={props.exercise1} />
-          <Part part={props.part2} exercise={props.exercise} />
+          <Part part={props.part2} exercise={props.exercise2} />
           <Part part={props.part3} exercise={props.exercise3} />
         </div>
       )
--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -16,7 +16,7 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises3} part3={part3} exercises3={exercises3}/>
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3}/>
       <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
     </div>
   )
