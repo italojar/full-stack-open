de48d9146383e0c031760cc71f07de2dd729fc1c
italojar <jaramilloarmijositalo@gmail.com>
2022-02-10 23:47:17 +0100

    chore: finish exercise 1.5 courseinfo. Passing an object of data between components

--- part1/courseinfo/src/Total.js
+++ part1/courseinfo/src/Total.js
@@ -2,7 +2,7 @@ const Total = (props) => {
     return (
         // 2 - 
         <p>
-           Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises} / 
+           Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises} 
         </p>
     )
 }
